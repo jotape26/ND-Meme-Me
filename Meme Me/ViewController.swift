6d408979c827f64546bd6b4878b58107e9e95450
João Leite <joaopleite26@gmail.com>
2018-10-08 21:50:00 -0300

    Initial Commit

---
+++ Meme Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meme Me
+//
+//  Created by João Leite on 08/10/18.
+//  Copyright © 2018 João Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
